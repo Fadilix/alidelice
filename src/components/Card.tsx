798bf4ca997e1357f20fec846f32a838c288ef36
Fadilix <iamfadile2@gmail.com>
2024-04-06 09:05:45 +0000

    added sick animation on hover (button)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,23 @@
+import "../scss/Card.scss"
+import star from "../assets/svgs/star.svg"
+
 const Card = () => {
     return (
-        <div className="card">
-            <div className="image"></div>
-            <p className="texte">Le Gâteau</p>
-            <p className="description">Découvrez Le Gateau d'AliDélice : une création culinaire exquise, mêlant harmonieusement saveurs et textures</p>
-            <button className="command">Commander</button>
+        <div className="card-container">
+            <div className="card">
+                <div className="image">
+                    <p className="description">Découvrez Le Gateau d'AliDélice : une création culinaire exquise, mêlant harmonieusement saveurs et textures</p>
+                </div>
+                <p className="text">Le Gâteau</p>
+                <div className="stars">
+                    <img src={star} alt="star-rating" />
+                    <img src={star} alt="star-rating" />
+                    <img src={star} alt="star-rating" />
+                    <img src={star} alt="star-rating" />
+                    <img src={star} alt="star-rating" />
+                </div>
+                <button className="command"><p>Commander</p></button>
+            </div>
         </div>
     )
 }
--- src/components/LeGateau.tsx
+++ src/components/LeGateau.tsx
@@ -1,8 +1,10 @@
 import Card from "./Card";
+import "../scss/LeGateau.scss"
 
 const LeGateau = () => {
     return (
-        <div>
+        <div className="legateau-container">
+            <h1>Découvrez Le Gâteau d'AliDélice</h1>
             <Card />
         </div>
     )
